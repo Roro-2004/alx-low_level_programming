5849b858fd524e9da81eb50823fb16f3cc1b7251
Roro-2004 <roaa.m.lotfy@gmail.com>
2023-08-11 15:43:09 -0700

    a

---
+++ 1-last_digit.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main -Entry point
+ *
+ * Description: A C program that prints the last digit and checks
+ *
+ * Return: always 0 (sucess)
+*/
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	int y = n % 10;
+
+	if (y > 5)
+		printf("Last digit of %d is %d and is greater than 5\n", n, y);
+	else if (y == 0)
+		printf("Last digit of %d is %d and is 0\n", n, y);
+	else if (y < 6)
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, y);
+	return (0);
+}
