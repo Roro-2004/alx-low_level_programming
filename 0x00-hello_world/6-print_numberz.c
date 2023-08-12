1c6d1793e7402af21d089b1f37ad0937ff16b731
Roro-2004 <roaa.m.lotfy@gmail.com>
2023-08-11 17:29:46 -0700

    a

---
+++ 0x00-hello_world/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+/**
+ * main -Entry point
+ *
+ * Description: A C program that prints no from 0 to 9
+ *
+ * Return: 0 (success)
+ *
+*/
+int main(void)
+{
+	int i;
+
+	for (i = 0 ; i < 10 ; i++)
+	{
+	putchar(i - 48);
+	}
+	putchar('\n');
+	return (0);
+}
