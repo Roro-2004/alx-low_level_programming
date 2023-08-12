29e8c92ce0a13d04d8d1ca8f3479df75c2fca79c
Roro-2004 <roaa.m.lotfy@gmail.com>
2023-08-11 18:25:41 -0700

    a

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+/**
+ * main -Entry point
+ *
+ * Description: A C program that writes the possible combinations of single nos
+ *
+ * Return: 0 (success)
+ *
+*/
+int main(void)
+{
+	char x;
+
+	for (x = 0 ; x < 10 ; x++)
+	{
+	putchar(x + 48);
+	if (x < 9)
+	{
+	putchar(',');
+	putchar(' ');
+	}
+	}
+	putchar('\n');
+	return (0);
+}
