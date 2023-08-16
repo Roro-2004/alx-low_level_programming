b1ca67e9a4ffea402b65a6548fcb04ee0069f5b3
Roro-2004 <roaa.m.lotfy@gmail.com>
2023-08-16 04:55:12 -0700

    a

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+/**
+ * main -entry point
+ *
+ * Description: a c program tht prints using printf
+ *
+ * Return: 0 (success)
+ *
+*/
+int main(void)
+{
+	printf("_putchar\n");
+	return (0);
+}
