3fcf36abf9623435a21af72e0932a67c07e72101
Roro-2004 <roaa.m.lotfy@gmail.com>
2023-08-22 14:34:39 -0700

    a

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * print_last_digit - func that prints the last digit of no
+ *
+ * @m: the no
+ *
+ * Return: the last digit
+*/
+int print_last_digit(int m)
+{
+	m %= 10;
+	return (m);
+
+}
