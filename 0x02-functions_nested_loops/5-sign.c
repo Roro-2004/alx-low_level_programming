14582f2b88e5df11d958825421948c6a010b7089
Roro-2004 <roaa.m.lotfy@gmail.com>
2023-08-22 06:57:03 -0700

    a

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,25 @@
+#include"main.h"
+/**
+ * print_sign - tells weather the no is + , - or 0
+ *
+ * @n: the int
+ *
+ * Return: 0 no is zero  1 the no is pos -1 the no is neg
+*/
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		return (1);
+		putchar ('+');
+	}
+	else if (n < 0)
+	{
+		return (-1);
+		putchar ('-');
+	}
+	else
+		return (0);
+		putchar ('0');
+
+}
