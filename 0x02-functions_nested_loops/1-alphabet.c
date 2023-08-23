638835538f64c05b95d8f03c56cd86ce9ed6fb4e
Roro-2004 <roaa.m.lotfy@gmail.com>
2023-08-23 15:34:56 -0700

    a

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,13 +1,14 @@
+#include <unistd.h>
 #include "main.h"
 
 /**
- * print_alphabet - utilies _putchar to print alphabets from a - z
-*/
-void print_alphabet(void)
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _putchar(char c)
 {
-	int j;
-
-	for (j = 'a' ; j <= 'z' ; j++)
-		_putchar(j);
-	_putchar('\n');
+return (write(1, &c, 1));
 }
--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -5,28 +5,35 @@
 */
 void times_table(void)
 {
-	int i, j, x;
+	int i, j, x, y;
 
 	for (i = 0; i <= 9 ; i++)
 	{
 		for (j = 0; j <= 9 ; j++)
 	{
 		x = i * j;
+		y = i * (j + 1);
 		if (j < 9)
 		{
-		if (x <= 9)
+		if (x <= 9 && y <= 9)
 		{
 		_putchar(x + 48);
 		_putchar(44);
 		_putchar(' ');
 		_putchar(' ');
 		}
-		else
+		else if (x > 9)
 		{
 		_putchar((x / 10) + 48);
 		_putchar((x % 10) + 48);
 		_putchar(44);
 		_putchar(' ');
+		}
+		else
+		{
+			_putchar(x + 48);
+			_putchar(44);
+			_putchar(' ');
 		}
 		}
 		else
