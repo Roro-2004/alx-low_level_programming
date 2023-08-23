7e3d3a808d64ebcbc46ea3d96bec6b36427d78b4
Roro-2004 <roaa.m.lotfy@gmail.com>
2023-08-23 14:16:16 -0700

    a

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,39 @@
+#include"main.h"
+/**
+ * print_to_98 - func that prints nos till 98
+ *
+ * @n: the start
+*/
+void print_to_98(int n)
+{
+	int i;
+
+	if (n <= 98)
+	{
+	for (i = n; i <= 98 ; i++)
+	{
+		printf("% d", i);
+		_putchar(44);
+		_putchar(' ');
+	}
+	_putchar('\n');
+	}
+	else
+	{
+		for (i = n; i >= 98; i--)
+		{
+			if (i != 98)
+			{
+			printf("% d", i);
+			_putchar(44);
+			_putchar(' ');
+			}
+			else
+			{
+			printf("98");
+			printf(" ");
+			}
+		}
+		_putchar('\n');
+	}
+}
