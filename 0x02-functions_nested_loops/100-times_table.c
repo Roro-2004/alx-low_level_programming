1110c1781bc37d633581a9078c511d501ff8a382
Roro-2004 <roaa.m.lotfy@gmail.com>
2023-08-24 14:46:02 -0700

    a

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,35 @@
+#include"main.h"
+/**
+ * print_times_table - a func that prints time table of n
+ *
+ * @n: int
+*/
+void print_times_table(int n)
+{
+	int i, j, x, y;
+
+	for (i = 0; i <= n; i++)
+	{
+		for (j = 0; j <= n; j++)
+		{
+			x = i * j;
+			y = i * (j + 1);
+			if (j != n)
+			{
+			if (y > 9)
+			{
+				printf("%d, ", x);
+			}
+			else
+			{
+				printf("%d,  ", x);
+			}
+			}
+			else
+			{
+				printf("%d", x);
+			}
+		}
+		printf("\n");
+	}
+}
