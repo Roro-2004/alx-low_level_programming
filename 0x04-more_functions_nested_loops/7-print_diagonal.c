164b809e41d86646e5819b1441a600f4fa6c52fc
Roro-2004 <roaa.m.lotfy@gmail.com>
2023-08-28 12:58:47 -0700

    A3333333

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -11,13 +11,11 @@ void print_diagonal(int n)
 	{
 		for (i = 0; i < n; i++)
 		{
-			for (j = 0; j <= i; j++)
+			for (j = 0; j < i; j++)
 			{
 				_putchar(' ');
-				if (i == j)
-				_putchar(92);
-
 			}
+			_putchar (92);
 			_putchar ('\n');
 		}
 	}
