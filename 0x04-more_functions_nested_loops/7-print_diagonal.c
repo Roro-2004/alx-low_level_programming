5385b268a54694eaca0774d5e3e3b6b0a96e7e99
Roro-2004 <roaa.m.lotfy@gmail.com>
2023-08-28 12:16:05 -0700

    A3333333

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,34 @@
+#include"main.h"
+/**
+ * print_diagonal - a func prints \ for a digonal
+ * @n: int
+*/
+void print_diagonal(int n)
+{
+	int i, j, arr[10][10];
+
+	if (n > 0)
+	{
+		for (i = 0; i < n; i++)
+		{
+			for (j = 0; j < n; j++)
+			{
+				if (i == j)
+				{
+					arr[i][j] = 92;
+				}
+				else
+					arr[i][j] = 32;
+			}
+		}
+		for (i =0; i < n; i++){
+			for (j =0; j < n; j++)
+			{
+				_putchar(arr[i][j]);
+			}
+			_putchar ('\n');
+		}
+	}
+	else
+		_putchar('\n');
+}
