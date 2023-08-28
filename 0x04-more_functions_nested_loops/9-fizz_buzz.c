c15d29bcf49c5418fd7f529208b1bf2c5b6c9481
Roro-2004 <roaa.m.lotfy@gmail.com>
2023-08-28 15:02:30 -0700

    A3333333

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -15,15 +15,15 @@ int i;
 for (i = 1; i <= 100; i++)
 {
 if ((i % 3 == 0) && (i % 5 == 0))
-printf("FizzBuzz ");
+printf("FizzBuzz");
 else if (i % 3 == 0)
-printf("Fizz ");
+printf("Fizz");
 else if (i % 5 == 0)
-printf("Buzz ");
-else if (i == 100)
 printf("Buzz");
 else
-printf("%d ", i);
+printf("%d", i);
+if (i < 100)
+putchar(' ');
 }
 printf("\n");
 return (0);
