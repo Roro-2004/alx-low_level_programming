b25787536337b599097460e5f119490b93d6f9e8
Roro-2004 <roaa.m.lotfy@gmail.com>
2023-08-30 15:43:06 -0700

    A3333333

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,25 +1,24 @@
 #include"main.h"
 /**
- * _print_rev - a func that prints a string
- * @str: a string
+ * print_rev - a func that prints a string
+ * @s: a string
 */
 void print_rev(char *s)
 {
-	int sum = 0, i = 0, j;
+int sum = 0, i = 0, j;
 
-	while (s[i] != 0)
-	{
-		if (s[i])
-			sum++;
-		else
-			break;
-		i++;
-	}
-	for (j = sum - 1; j >= 0; j--)
-	{
-		if (j != 0)
-			_putchar(s[j]);
-		else
-			_putchar ('\n');
-	}
+while (s[i] != 0)
+{
+if (s[i])
+sum++;
+else
+break;
+i++;
+}
+for (j = sum; j >= 0; j--)
+{
+_putchar(s[j]);
+if (j == 0)
+_putchar ('\n');
+}
 }
