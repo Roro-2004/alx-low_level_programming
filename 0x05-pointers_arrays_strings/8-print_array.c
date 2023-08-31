f176632c8c3a50866f85b04ad3c7d0aad4b060b5
Roro-2004 <roaa.m.lotfy@gmail.com>
2023-08-30 18:25:35 -0700

    A3333333

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -9,6 +9,8 @@ void print_array(int *a, int n)
 {
 	int i;
 
+	if (n > 0)
+	{
 	for (i = 0; i < n; i++)
 	{
 		if (i != n - 1)
@@ -19,4 +21,7 @@ void print_array(int *a, int n)
 			printf("\n");
 		}
 	}
+	}
+	else
+		printf("\n");
 }
