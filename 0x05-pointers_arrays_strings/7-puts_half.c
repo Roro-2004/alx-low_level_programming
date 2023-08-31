644fd2793b1acc7330fb3fe736bd921545350166
Roro-2004 <roaa.m.lotfy@gmail.com>
2023-08-30 17:51:35 -0700

    A3333333

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,28 @@
+#include"main.h"
+#include<stdio.h>
+/**
+ * puts_half - func
+ * @str: array
+*/
+void puts_half(char *str)
+{
+	int i = 0, len = 0, n;
+
+	while (str[i] != '\0')
+	{
+		if (str[i])
+			len++;
+		else
+			break;
+		i++;
+	}
+	if (len % 2 == 0)
+		n = len / 2;
+	else
+		n = (len - 1) / 2;
+	for (i = n; str[i]; i++)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
