20870d32db058709ad01de78b2b95d24941dda6b
Roro-2004 <roaa.m.lotfy@gmail.com>
2023-09-04 06:48:28 -0700

    A3333333

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,21 @@
+#include"main.h"
+/***/
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i = 0, j = 0;
+
+	while (src[i] !='\0')
+	{
+		if (src[i])
+			i++;
+		else
+			break;
+	}
+	for (j = 0; j < n && src[j] != '\0'; j++)
+	{
+		dest[j] = src[j];
+	}
+	for (; j < n; j++)
+		dest[i] = '\0';
+	return(dest);
+}
