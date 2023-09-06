6f39f14b68752500a617cfe14d1f5b214504fc2d
Roro-2004 <roaa.m.lotfy@gmail.com>
2023-09-06 13:55:21 -0700

    A3333333

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,17 @@
+#include"main.h"
+/**
+ * reverse_array - a func
+ * @a: array
+ * @n: no of elements
+*/
+void reverse_array(int *a, int n)
+{
+	int temp, i;
+
+	for (i = 0; i < n / 2; i++)
+	{
+		temp = a[i];
+		a[i] = a[n - 1 - i];
+		a[n - 1 - i] = temp;
+	}
+}
