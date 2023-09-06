6106c9f2f88669f6c3d54e1264b7764ae7d7c65f
Roro-2004 <roaa.m.lotfy@gmail.com>
2023-09-06 13:35:24 -0700

    A3333333

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -7,13 +7,15 @@
 */
 int _strcmp(char *s1, char *s2)
 {
-	int x;
+	int x = 0, i = 0;
 
-	if (s1[1] > s2[1])
-		x = 15;
-	else if (s1[1] < s2[1])
-		x = -15;
-	else
-		x = 0;
+	while (s1[i])
+	{
+		if (s1[i] != s2[i])
+		{x = (((int)s1[i] - 48) - ((int)s2[i] - 48));
+			break;
+		}
+		i++;
+	}
 	return (x);
 }
