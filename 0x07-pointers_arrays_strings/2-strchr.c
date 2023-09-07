87b60d7402c5905fd312f2dae55e130798e6f306
Roro-2004 <roaa.m.lotfy@gmail.com>
2023-09-07 14:04:59 -0700

    A3333333

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,26 @@
+#include"main.h"
+/**
+ * *_strchr - func
+ * @s: array
+ * @c: char
+ * Return: x
+*/
+char *_strchr(char *s, char c)
+{
+	int i = 0, j;
+
+	while (s[i] != '\0')
+	{
+		if (s[i])
+			i++;
+		else
+			break;
+	}
+	for (j = 0; j < i; j++)
+	{
+		if (s[j] == c)
+		{
+			return (s + j);
+		}
+	}return ('\0');
+}
