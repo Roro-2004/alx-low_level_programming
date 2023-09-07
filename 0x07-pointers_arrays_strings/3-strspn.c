59e586a326d0e0284c7ddc0a0d0ff3f32c4b3301
Roro-2004 <roaa.m.lotfy@gmail.com>
2023-09-07 14:27:57 -0700

    A3333333

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,28 @@
+#include"main.h"
+/**
+ * _strspn - a func
+ * @s: array
+ * @accept: 2nd array
+ * Return: no
+*/
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int i = 0, j, k, c = 0;
+
+	while (accept[i] != '\0')
+	{
+		if (accept[i])
+			i++;
+		else
+			break;
+	}
+	for (j = 0; j <= i; j++)
+	{
+		for (k = 0; k < i; k++)
+		{
+			if (accept[k] == s[j])
+				c++;
+		}
+	}
+	return (c);
+}
