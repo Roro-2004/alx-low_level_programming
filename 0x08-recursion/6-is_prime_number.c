fec7af1db98773d7bd14ba8eafac6bbe3c540bb4
Roro-2004 <roaa.m.lotfy@gmail.com>
2023-09-08 15:50:20 -0700

    A3333333

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,25 @@
+#include"main.h"
+/**
+ * check - func
+ * @n: par
+ * @o : par
+ * Return: 0
+*/
+int check(int n, int o)
+{
+	if (o >= n && n > 1)
+		return (0);
+	else if (n % o == 0 || n <= 1)
+		return (1);
+	else
+		return (check(n, o + 1));
+}
+/**
+ * is_prime_number - func
+ * @n: par
+ * Return: 0
+*/
+int is_prime_number(int n)
+{
+	return (check(n, 2));
+}
