595ea2df9a0f4458ad6e061543def6778c30a0a6
Roro-2004 <roaa.m.lotfy@gmail.com>
2023-09-08 11:34:09 -0700

    A3333333

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,16 @@
+#include"main.h"
+#include<stdio.h>
+/**
+ * _puts_recursion - a func
+ * @s: array
+*/
+void _puts_recursion(char *s)
+{
+	if (*s)
+	{
+		_putchar (*s);
+		_puts_recursion(s + 1);
+	}
+	else
+		return;
+}
