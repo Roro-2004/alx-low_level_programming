85211bc96aca7c5a619546fb575d3d1eace49081
Roro-2004 <roaa.m.lotfy@gmail.com>
2023-09-10 13:18:54 -0700

    A3333333

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,17 @@
+#include"main.h"
+/**
+ * _strcpy - func
+ * @dest: arr
+ * @src: arr
+ * Return: dest
+*/
+char *_strcpy(char *dest, char *src)
+{
+	int i = -1;
+
+	do {
+		i++;
+		dest[i] = src[i];
+	} while (src[i] != '\0')
+	return (dest);
+}
