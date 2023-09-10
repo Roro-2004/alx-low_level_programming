6c18f65dcbe7f025fafb8d84e724e52d4a05f382
Roro-2004 <roaa.m.lotfy@gmail.com>
2023-09-10 14:35:56 -0700

    A3333333

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+/**
+ * main - entry point
+ * @argc: a
+ * @argv: array
+ * Return: 0
+*/
+int main(int argc, char const *argv[])
+{
+	(void)argc;
+	printf("%s\n", argv[0]);
+	return (0);
+}
