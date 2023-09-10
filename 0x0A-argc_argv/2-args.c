670ace53941b1170903adde21f1f3fe656190c2d
Roro-2004 <roaa.m.lotfy@gmail.com>
2023-09-10 14:52:45 -0700

    A3333333

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+/**
+ * main - entry point
+ * @argc: a
+ * @argv: array
+ * Return: 0
+*/
+int main (int argc, char const *argv[])
+{
+	int i = 0;
+
+	while (argc--)
+	{
+		printf("%s\n", argv[i]);
+		i++;
+	}
+	return (0);
+}
