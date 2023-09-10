3ecb08e515453ab040de6dfff1272898866b19d9
Roro-2004 <roaa.m.lotfy@gmail.com>
2023-09-10 15:02:30 -0700

    A3333333

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+/**
+ * main - entry point
+ * @argc: int
+ * @argv: arra
+ * Return: 0
+*/
+int main(int argc, char *argv[])
+{
+	int sum;
+
+	if (argc == 3)
+	{
+		sum = atoi(argv[1]) * atoi(argv[2]);
+		printf("%d\n", sum);
+	}
+	else
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	return (0);
+}
