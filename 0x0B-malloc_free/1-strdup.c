da701246bd5101ba93f46e5823f9a115229a1ccc
Roro-2004 <roaa.m.lotfy@gmail.com>
2023-09-11 14:45:51 -0700

    A3333333

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,30 @@
+#include"main.h"
+#include<stdlib.h>
+/**
+ * _strdup - func
+ * @str: char
+ * Return: ptr
+*/
+char *_strdup(char *str)
+{
+	char *ptr = NULL;
+
+	unsigned int i = 0, j;
+
+	while (str[i] != '\0')
+	{
+		if (str[i])
+			i++;
+		else
+			break;
+	}
+	ptr = (char *)malloc(i * sizeof(char));
+	if (i == 0 || ptr == NULL)
+		return ('\0');
+	if (i > 0)
+	{
+		for (j = 0; j < i; j++)
+			ptr[j] = str[j];
+	}
+	return (ptr);
+}
