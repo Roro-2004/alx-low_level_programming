8bc4600881b9cde7a8140fab2b2551f4830d8c7c
Roro-2004 <roaa.m.lotfy@gmail.com>
2023-09-12 20:03:02 -0400

    A3333333

---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,1 @@
+ya rab ostoorha 3lya ana 3abeta
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - dog struct
+ * @name: dog's name
+ * @age: dog's age
+ * @owner: dog's owner
+ *
+ * Description: a struct for a dog taking info
+*/
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
