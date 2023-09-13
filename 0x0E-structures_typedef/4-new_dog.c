022b401db737f101b35e16f1619a1e926e3c0ffb
Roro-2004 <roaa.m.lotfy@gmail.com>
2023-09-12 21:58:55 -0400

    A3333333

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,68 @@
+#include"dog.h"
+#include<stdlib.h>
+/**
+ * strlen - func
+ * @str: arr
+ * Return: len
+*/
+int _strlen(const char *str)
+{
+	int len = 0;
+	while (*str++)
+		len++;
+	return (len);
+}
+/**
+ * strcpy - func
+ * @dest: arr
+ * @str: arr
+ * Return: arr
+*/
+char *_strcpy(char *dest, char *str)
+{
+	int i;
+
+	for (i = 0; str[i]; i++)
+		dest[i] = str[i];
+	dest[i] = '\0';
+
+	return (dest);
+}
+/**
+ * new_dog - func
+ * @name: n
+ * @age: age
+ * @owner: o
+ * Return: struct
+*/
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *dog;
+
+	if (!name || age < 0 || !owner)
+		return (NULL);
+
+	dog = (dog_t *)malloc(sizeof(dog_t));
+	if (dog == NULL)
+		return (NULL);
+
+	dog->name = malloc(sizeof(char) * (_strlen(name) + 1));
+	if ((*dog).name == NULL)
+	{
+		free(dog);
+		return (NULL);
+	}
+	dog->owner = malloc(sizeof(char) * (_strlen(owner) + 1));
+	if ((*dog).owner == NULL)
+	{
+		free(dog->name);
+		free(dog);
+		return (NULL);
+	}
+
+	dog->name = _strcpy(dog->name, name);
+	dog->age = age;
+	dog->owner = _strcpy(dog->owner, owner);
+
+	return (dog);
+}
