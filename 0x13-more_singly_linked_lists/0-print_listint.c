09b78a2d09ef9179c4d64a24969c9b9d1fbfe09b
Roro-2004 <roaa.m.lotfy@gmail.com>
2023-09-25 04:51:54 -0400

    u are losing me

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+/***/
+size_t print_listint(const listint_t *h)
+{
+	size_t count = 0;
+	while (h != NULL)
+	{
+		count++;
+		printf("%d\n", h->n);
+		h = h->next;
+	}
+	return (count);
+}
